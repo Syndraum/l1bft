67093228c424b1201e7340f87f410e4d2eb0014b
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-10-08 16:25:26 +0200

    Adding atoi function, no overflow

---
+++ ft_atoi.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roalvare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 15:22:14 by roalvare          #+#    #+#             */
+/*   Updated: 2019/10/08 16:01:49 by roalvare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_return(char *str, int count)
+{
+	int result;
+	int i;
+
+	result = 0;
+	i = 0;
+	if (count == 0)
+		return (0);
+	while (i < count)
+	{
+		result *= 10;
+		result += (*(str + i) - '0');
+		i++;
+	}
+	return (result);
+}
+
+int	ft_is_white_space(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int count;
+	int signe;
+
+	i = 0;
+	count = 0;
+	signe = 1;
+	while (str[i] && ft_is_white_space(str[i]))
+		i++;
+	if (str[i] == '-')
+	{
+		signe *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		count++;
+		i++;
+	}
+	return (ft_return((char*)&str[i - count], count) * signe);
+}
