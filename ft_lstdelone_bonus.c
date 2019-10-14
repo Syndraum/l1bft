2c197928ab2a2d5c615c090e5bee6aae1a17ae97
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-10-14 13:00:59 +0200

    Correct lstdelone and lstcler

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: syndraum <syndraum@student.42.fr>          +#+  +:+       +#+        */
+/*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 13:40:56 by syndraum          #+#    #+#             */
-/*   Updated: 2019/10/13 13:42:38 by syndraum         ###   ########.fr       */
+/*   Updated: 2019/10/14 12:53:26 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	del(lst->next);
+	del(lst->content);
 	free(lst);
 }
