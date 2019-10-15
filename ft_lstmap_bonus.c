46ae1f6dbae1b208d2e7345cbf7bd6dbaedd70df
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-10-15 16:13:28 +0200

    Optimize lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 14:19:54 by syndraum          #+#    #+#             */
-/*   Updated: 2019/10/14 12:37:28 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/10/15 16:10:24 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
 {
 	t_list	*lst_rtr;
+	t_list	*cursor;
 
-	lst_rtr = NULL;
+	if (!(lst_rtr = ft_calloc(ft_lstsize(lst), sizeof(t_list))))
+		return (NULL);
+	cursor = lst_rtr;
 	while (lst != NULL)
 	{
-		ft_lstadd_back(&lst_rtr, ft_lstnew(f(lst->content)));
+		cursor->content = f(lst->content);
+		cursor->next = NULL;
+		if (cursor != lst_rtr)
+			ft_lstadd_back(&lst_rtr, cursor);
+		cursor++;
 		lst = lst->next;
 	}
 	return (lst_rtr);
