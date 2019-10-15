24bd74fadd2f7a474abc3960d3026dcb84782800
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-10-15 13:06:16 +0200

    Correct strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,41 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roalvare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 14:58:50 by roalvare          #+#    #+#             */
-/*   Updated: 2019/10/08 14:58:54 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/10/15 13:02:48 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
-	int		j;
-	size_t	lenght_dest;
-	size_t	lenght_src;
+	size_t	len_dst;
+	size_t	len_src;
 
-	i = 0;
-	j = 0;
-	lenght_dest = ft_strlen(dst);
-	lenght_src = ft_strlen(src);
-	if (dstsize == 0)
-		return (lenght_src);
-	while (dst[i])
-	{
-		i++;
-	}
-	while (i < dstsize - 1 && src[j])
-	{
-		dst[i] = src[j];
-		i++;
-		j++;
-	}
-	if (lenght_dest > dstsize)
-		return (lenght_src + dstsize);
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (len_dst >= dstsize)
+		return (len_src + dstsize);
+	if (len_src < dstsize - len_dst)
+		ft_strlcpy(dst + len_dst, src, len_src + 1);
 	else
-		return (lenght_dest + lenght_src);
+	{
+		ft_strlcpy(dst + len_dst, src, dstsize - len_dst);
+	}
+	return (len_dst + len_src);
 }
