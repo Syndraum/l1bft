9579a97c5cf3d5bfe98e4b7c9f4365b67d6c2200
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-10-21 11:44:00 +0200

    Modify lsmap to fit the subject

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,27 +6,27 @@
 /*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 14:19:54 by syndraum          #+#    #+#             */
-/*   Updated: 2019/10/15 16:10:24 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/10/21 11:33:57 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*lst_rtr;
 	t_list	*cursor;
 
-	if (!(lst_rtr = ft_calloc(ft_lstsize(lst), sizeof(t_list))))
-		return (NULL);
+	lst_rtr = NULL;
 	cursor = lst_rtr;
 	while (lst != NULL)
 	{
-		cursor->content = f(lst->content);
-		cursor->next = NULL;
-		if (cursor != lst_rtr)
-			ft_lstadd_back(&lst_rtr, cursor);
-		cursor++;
+		if (!(cursor = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&lst_rtr, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&lst_rtr, cursor);
 		lst = lst->next;
 	}
 	return (lst_rtr);
