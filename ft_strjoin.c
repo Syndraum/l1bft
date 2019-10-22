e087e82d1fad57973cbcd1e3eea9722aeeb36d05
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-10-22 16:32:49 +0200

    change retur if alloc fail

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 13:32:50 by roalvare          #+#    #+#             */
-/*   Updated: 2019/10/22 15:39:03 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/10/22 16:30:06 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len = ft_strlen(s1) + ft_strlen(s2);
 	str = NULL;
 	if (!(str = ft_calloc(sizeof(char), len + 1)))
-		return (str);
+		return (NULL);
 	ft_strlcat(str, s1, ft_strlen(s1) + 1);
 	ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
 	return (str);
