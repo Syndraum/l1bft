f61afbe68f57ac985dd3d2a0c26d9144b7e1a8a4
Syndraun <roms2111@gmail.com>
2020-05-01 18:15:08 +0200

    opti lstlast

--- list/ft_lstlast.c
+++ list/ft_lstlast.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 12:48:13 by roalvare          #+#    #+#             */
-/*   Updated: 2019/10/12 12:51:08 by roalvare         ###   ########.fr       */
+/*   Updated: 2020/05/01 16:10:27 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst && lst->next)
+	if (!lst)
+		return (NULL);
+	while (lst->next)
 		lst = lst->next;
 	return (lst);
 }
